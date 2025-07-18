0d10235768e67459bcee0af58bcd01d9373a499f
Faris <mustang.turk@hotmail.com>
2025-07-18 14:12:49 +0300

    background.js

--- background.js
+++ background.js
@@ -1,11 +1,9 @@
-// Eklenti ikonuna tıklandığında rapor sayfasını açar
 chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.create({
     url: 'report.html'
   });
 });
 
-// Eklenti ilk kez kurulduğunda bir kereliğine hoş geldiniz sayfasını açar
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.tabs.create({
